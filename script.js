56c752d860ebea3e004aa617f8538807923f2dae
Assia <122679741+Assia123-oss@users.noreply.github.com>
2026-04-14 10:12:37 +0200

    Add function to create and append task items

---
+++ script.js
@@ -0,0 +1,6 @@
+function addTask() {
+  let task = document.getElementById("task").value;
+  let li = document.createElement("li");
+  li.innerText = task;
+  document.getElementById("list").appendChild(li);
+}
