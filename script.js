c9e24fb30ab82c8bbd80386f56aa27c61403680d
Assia <122679741+Assia123-oss@users.noreply.github.com>
2026-05-07 17:46:37 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function addTask() {
-  let task = document.getElementById("task").value;
-  let li = document.createElement("li");
-  li.innerText = task;
-  document.getElementById("list").appendChild(li);
-}
